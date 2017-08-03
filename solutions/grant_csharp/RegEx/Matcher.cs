ba2f844786b01d2e54b032ae1bcfedf624426fa8
Grant Crofton <grantac@hotmail.com>
2017-08-03 08:35:33 +0100

    Added a bit more

--- solutions/grant_csharp/RegEx/Matcher.cs
+++ solutions/grant_csharp/RegEx/Matcher.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegEx
 {
     public class Matcher
     {
+        const int ASCII_ZERO = 48;
+        const int ASCII_NINE = 57;
+
+        private static Dictionary<String, Predicate<char>> tokenMatchers = new Dictionary<string, Predicate<char>> {
+            {@".", inputChar => true },
+            {@"\d", inputChar => inputChar >= ASCII_ZERO && inputChar <= ASCII_NINE },
+            {@"\D", inputChar => inputChar > ASCII_NINE || inputChar < ASCII_ZERO }
+        };
+
         public static bool Matches(string input, string pattern) {
             int cursor = 0;
 
-            foreach(var character in pattern) {
-                if(input.ElementAt(cursor) == character || character == '.' ) {
+            foreach(var token in GetTokens(pattern)) {
 
+                var inputChar = input.Substring(cursor, 1).ElementAt(0);
+
+                if(inputChar.ToString() == token) { // literal
                     cursor++;
+                }
+                else if (tokenMatchers.Keys.Contains(token) && 
+                         tokenMatchers[token].Invoke(inputChar)) {
+                        cursor++;
                 }
                 else {
                         return false;
@@ -22,6 +35,17 @@ namespace RegEx
             }
 
             return true;
+        }
+
+        private static IEnumerable<string> GetTokens(string pattern) {
+            string current = "";
+            foreach (var character in pattern) {
+                current += character;
+                if (character != '\\') {
+                    yield return current;
+                    current = "";
+                }
+            }
         }
     }
 }
--- solutions/grant_csharp/RegEx/Tests.cs
+++ solutions/grant_csharp/RegEx/Tests.cs
@@ -14,6 +14,42 @@ namespace RegEx {
             Assert.IsTrue(Regex.IsMatch("hi mum", ".*"));
             Assert.IsTrue(Regex.IsMatch("hi mum", "hi mum"));
             Assert.IsTrue(Regex.IsMatch("hi mum", ""));
+
+            Assert.IsTrue(Regex.IsMatch("abc", "abc"), "abc");
+            Assert.IsFalse(Regex.IsMatch("xyz", "abc"), "abc");
+
+            Assert.IsTrue(Regex.IsMatch("axcye", "a.c.e"), "a.c.e");
+            Assert.IsFalse(Regex.IsMatch("abbbcde", "a.c.e"), "a.c.e");
+
+            Assert.IsTrue(Regex.IsMatch("abc129", @"abc12\d"), @"abc12\d");
+            Assert.IsFalse(Regex.IsMatch("abc12z", @"abc12\d"), @"abc12\d");
+
+            Assert.IsTrue(Regex.IsMatch("abc12z", @"abc12\D"), @"abc12\D");
+            Assert.IsFalse(Regex.IsMatch("abc123", @"abc12\D"), @"abc12\D");
+
+            Assert.IsTrue(Regex.IsMatch("abz", @"ab\w"), @"ab\w");
+            Assert.IsFalse(Regex.IsMatch("ab:", @"ab\w"), @"ab\w");
+
+            Assert.IsTrue(Regex.IsMatch("hi mum", @"hi\smum"), @"hi\smum");
+            Assert.IsFalse(Regex.IsMatch("himum", @"hi\smum"), @"hi\smum");
+
+            Assert.IsTrue(Regex.IsMatch("xxxxxyz", @"x+yz"), @"x+yz");
+            Assert.IsFalse(Regex.IsMatch("yz", @"x+yz"), @"x+yz");
+
+            Assert.IsTrue(Regex.IsMatch("yz", @"x*yz"), @"x*yz");
+            Assert.IsFalse(Regex.IsMatch("abc", @"x*yz"), @"x*yz");
+
+            Assert.IsTrue(Regex.IsMatch("xxxyz", @"x{3}yz"), @"x{3}yz");
+            Assert.IsFalse(Regex.IsMatch("xxyz", @"x{3}yz"), @"x{3}yz");
+
+            Assert.IsTrue(Regex.IsMatch("b99", @"[abc]99"), @"[abc]99");
+            Assert.IsFalse(Regex.IsMatch("d99", @"[abc]99"), @"[abc]99");
+
+            Assert.IsTrue(Regex.IsMatch("abc123", @"^abc"), @"^abc");
+            Assert.IsFalse(Regex.IsMatch("123abc", @"^abc"), @"^abc");
+
+            Assert.IsTrue(Regex.IsMatch("123abc", @"abc$"), @"abc$");
+            Assert.IsFalse(Regex.IsMatch("abc123", @"abc$"), @"abc$");
         }
 
         [Test]
@@ -21,14 +57,21 @@ namespace RegEx {
             Assert.IsTrue(Matcher.Matches("hi mum", "hi mum"));
         }
 
-        //[Test]
-        //public void matches_when_exact_string_exists_in_input() {
-        //    Assert.IsTrue(Matcher.Matches("hi mum", "i m"));
-        //}
-
         [Test]
         public void dot_matches_any_character() {
             Assert.IsTrue(Matcher.Matches("hi mum", ".i.m.m"));
+        }
+
+        [Test]
+        public void backslash_d_matches_any_digit() {
+            Assert.IsTrue(Matcher.Matches("12345678", @"1\d3\d5\d7\d"));
+            Assert.IsFalse(Matcher.Matches("12z", @"12\d"));
+        }
+
+        [Test]
+        public void backslash_D_matches_any_non_digit() {
+            Assert.IsTrue(Matcher.Matches("abc123x-!", @"abc123\D\D\D"));
+            Assert.IsFalse(Matcher.Matches("123", @"\D\D\D"));
         }
     }
 }
